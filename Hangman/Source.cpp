6eba11e6081cef9f475dff9e96ab9465b2e4181c
Felipe Gomez <76770518+Fgomezcode@users.noreply.github.com>
2021-12-07 02:48:07 -0800

    Added checks to user input.
    Added a while loop in Source.cpp to validify user input - checks user input and makes sure it is a valid option so the game loop does not break.

--- Hangman/Source.cpp
+++ Hangman/Source.cpp
@@ -5,6 +5,7 @@ int main()
 {
     int length = 0;
     char again = 'Y';
+    bool isValid = false;
 
     do {
         vector<Word> words = createList();
@@ -13,10 +14,21 @@ int main()
 
         display(words, randomNumber(length));
 
-        cout << "\nAgain? (Y/N):\t";
-        cin >> again;
+        // prompt the user for valid input, and limit the input to two valid choices
+        do {
+            cout << "\nAgain? (Y/N):\t";
+            cin >> again;
 
-        again = toupper(again);
+            again = toupper(again);
+
+            if (again == 'Y' || again == 'N') {
+                isValid = true;
+            }
+            else {
+                isValid = false;
+            }
+        } while (isValid== false);
+
 
     } while (again == 'Y');
 
